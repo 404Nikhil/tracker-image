bc1381c8af3a30050a5917b785092ace69116c67
404Nikhil <nicksd111@gmail.com>
2024-11-10 18:45:53 +0530

    xcyiasivf

--- functions/update.js
+++ functions/update.js
@@ -1,7 +1,70 @@
 import { NhostClient } from "@nhost/nhost-js";
 
+const accessToken = process.env.NHOST_ADMIN_SECRET;
 const backendUrl = process.env.NHOST_BACKEND_URL;
 
 const nhost = new NhostClient({
   backendUrl: backendUrl,
 });
+
+nhost.graphql.setAccessToken(accessToken);
+
+export default async (req, res) => {
+  const imgText = req.query.text;
+  console.log("imgText", imgText);
+
+  if (!imgText) {
+    return res.status(500).json({ error: "No image token provided" });
+  }
+
+  const GET_EMAIL_ID = `
+  query getId($text: String!) {
+    emails(where: {img_text: {_eq: $text}}) {
+      id
+      seen
+    }
+  }`;
+
+  const UPDATE_QUERY = `
+    mutation UpdateEmail($id: Int!, $date: timestamptz!) {
+      update_emails(where: {id: {_eq: $id}}, _set: {seen: true, seen_at: $date}) {
+        affected_rows
+      }
+    }`;
+
+  try {
+    const { data, error } = await nhost.graphql.request(GET_EMAIL_ID, {
+      text: imgText,
+    });
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    if (!data) {
+      return res.status(500).json({ error: "No email found" });
+    }
+
+    const emailId = data.emails[0].id;
+    const seen = data.emails[0].seen;
+
+    if (seen) {
+      return res.status(500).json({ error: "Kaam hogaya vai" });
+    }
+
+    const { data: updatedData, error: updateError } =
+      await nhost.graphql.request(UPDATE_QUERY, {
+        id: emailId,
+        date: new Date(),
+      });
+
+    if (updateError) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.status(404).send({ error: "Bye bye" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+};
